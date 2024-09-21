f85eb76ec2d6206afe4bdb3ba70825cb112d0064
Sriyan-Nakka <sriyannakka@gmail.com>
2024-09-21 18:53:14 +0530

    version 0.5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let playButton = document.getElementById("playButton");
+let playContainer = document.getElementById("playContainer");
 let gameStats = document.getElementById("gameStats");
 let dice1 = document.getElementById("dice1");
 let dice2 = document.getElementById("dice2");
@@ -10,6 +11,23 @@ let personsTurn = document.getElementById("personsTurn");
 let rolledNumber = document.getElementById("rolledNumber");
 let yourScoreNum = document.getElementById("yourScoreNum");
 let botScoreNum = document.getElementById("botScoreNum");
+
+let playerUsername = window.prompt(
+  "Please enter your username in the given textbox. This name will be used while playing the game to represent you."
+);
+console.log(playerUsername);
+
+if (playerUsername == null || playerUsername == "") {
+  for (let i = 0; i < 10000; i++) {
+    playerUsername = window.prompt(
+      "Please enter your username in the given textbox. This name will be used while playing the game to represent you."
+    );
+    console.log(playerUsername);
+    if (playerUsername != null && playerUsername != "") {
+      break;
+    }
+  }
+}
 
 yourScore = 0;
 botScore = 0;
@@ -25,10 +43,18 @@ function yourTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Your";
+      personsTurn.textContent = playerUsername + "'s";
       rolledNumber.textContent = "1";
       yourScore += 1;
+      document.querySelector("#rollButtonPlayer").style.display = "none";
+      document.querySelector("#rollButtonBot").style.display = "block";
       yourScoreNum.textContent = yourScore;
+      if (yourScore === goal) {
+        gameStats.style.display = "none";
+        playContainer.style.display = "block";
+        document.querySelector("#resultSpan").textContent =
+          "Congratulations, you have won! Tap play to play again.";
+      }
       break;
 
     case 2:
@@ -38,10 +64,18 @@ function yourTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Your";
+      personsTurn.textContent = playerUsername + "'s";
       rolledNumber.textContent = "2";
       yourScore += 2;
+      document.querySelector("#rollButtonPlayer").style.display = "none";
+      document.querySelector("#rollButtonBot").style.display = "block";
       yourScoreNum.textContent = yourScore;
+      if (yourScore === goal) {
+        gameStats.style.display = "none";
+        playContainer.style.display = "block";
+        document.querySelector("#resultSpan").textContent =
+          "Congratulations, you have won! Tap play to play again.";
+      }
       break;
 
     case 3:
@@ -51,10 +85,18 @@ function yourTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Your";
+      personsTurn.textContent = playerUsername + "'s";
       rolledNumber.textContent = "3";
       yourScore += 3;
+      document.querySelector("#rollButtonPlayer").style.display = "none";
+      document.querySelector("#rollButtonBot").style.display = "block";
       yourScoreNum.textContent = yourScore;
+      if (yourScore === goal) {
+        gameStats.style.display = "none";
+        playContainer.style.display = "block";
+        document.querySelector("#resultSpan").textContent =
+          "Congratulations, you have won! Tap play to play again.";
+      }
       break;
 
     case 4:
@@ -64,10 +106,18 @@ function yourTurn() {
       dice4.style.display = "block";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Your";
+      personsTurn.textContent = playerUsername + "'s";
       rolledNumber.textContent = "4";
       yourScore += 4;
+      document.querySelector("#rollButtonPlayer").style.display = "none";
+      document.querySelector("#rollButtonBot").style.display = "block";
       yourScoreNum.textContent = yourScore;
+      if (yourScore === goal) {
+        gameStats.style.display = "none";
+        playContainer.style.display = "block";
+        document.querySelector("#resultSpan").textContent =
+          "Congratulations, you have won! Tap play to play again.";
+      }
       break;
 
     case 5:
@@ -77,10 +127,18 @@ function yourTurn() {
       dice4.style.display = "none";
       dice5.style.display = "block";
       dice6.style.display = "none";
-      personsTurn.textContent = "Your";
+      personsTurn.textContent = playerUsername + "'s";
       rolledNumber.textContent = "5";
       yourScore += 5;
+      document.querySelector("#rollButtonPlayer").style.display = "none";
+      document.querySelector("#rollButtonBot").style.display = "block";
       yourScoreNum.textContent = yourScore;
+      if (yourScore === goal) {
+        gameStats.style.display = "none";
+        playContainer.style.display = "block";
+        document.querySelector("#resultSpan").textContent =
+          "Congratulations, you have won! Tap play to play again.";
+      }
       break;
 
     case 6:
@@ -90,10 +148,18 @@ function yourTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "block";
-      personsTurn.textContent = "Your";
+      personsTurn.textContent = playerUsername + "'s";
       rolledNumber.textContent = "6";
       yourScore += 6;
+      document.querySelector("#rollButtonPlayer").style.display = "none";
+      document.querySelector("#rollButtonBot").style.display = "block";
       yourScoreNum.textContent = yourScore;
+      if (yourScore === goal) {
+        gameStats.style.display = "none";
+        playContainer.style.display = "block";
+        document.querySelector("#resultSpan").textContent =
+          "Congratulations, you have won! Tap play to play again.";
+      }
       break;
   }
 }
@@ -109,7 +175,7 @@ function botTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Bots";
+      personsTurn.textContent = "Bot's";
       rolledNumber.textContent = "1";
       botScore += 1;
       botScoreNum.textContent = botScore;
@@ -122,7 +188,7 @@ function botTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Bots";
+      personsTurn.textContent = "Bot's";
       rolledNumber.textContent = "2";
       botScore += 2;
       botScoreNum.textContent = botScore;
@@ -135,7 +201,7 @@ function botTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Bots";
+      personsTurn.textContent = "Bot's";
       rolledNumber.textContent = "3";
       botScore += 3;
       botScoreNum.textContent = botScore;
@@ -148,7 +214,7 @@ function botTurn() {
       dice4.style.display = "block";
       dice5.style.display = "none";
       dice6.style.display = "none";
-      personsTurn.textContent = "Bots";
+      personsTurn.textContent = "Bot's";
       rolledNumber.textContent = "4";
       botScore += 4;
       botScoreNum.textContent = botScore;
@@ -161,7 +227,7 @@ function botTurn() {
       dice4.style.display = "none";
       dice5.style.display = "block";
       dice6.style.display = "none";
-      personsTurn.textContent = "Bots";
+      personsTurn.textContent = "Bot's";
       rolledNumber.textContent = "5";
       botScore += 5;
       botScoreNum.textContent = botScore;
@@ -174,7 +240,7 @@ function botTurn() {
       dice4.style.display = "none";
       dice5.style.display = "none";
       dice6.style.display = "block";
-      personsTurn.textContent = "Bots";
+      personsTurn.textContent = "Bot's";
       rolledNumber.textContent = "6";
       botScore += 6;
       botScoreNum.textContent = botScore;
@@ -186,16 +252,16 @@ function botTurn() {
 playButton.onclick = function () {
   let goal = document.getElementById("goal").value;
   if (goal == null || goal < 15 || goal > 500) {
-    document.querySelector("#invalidNumberSpan").textContent =
+    document.querySelector("#resultSpan").textContent =
       "Invalid number, please try again.";
     setTimeout(() => {
-      document.querySelector("#invalidNumberSpan").textContent = "";
+      document.querySelector("#resultSpan").textContent = "";
     }, 3000);
   } else {
     goal = Number(goal);
     console.log(goal);
     gameStats.style.display = "block";
-    playButton.style.display = "none";
+    playContainer.style.display = "none";
     yourTurn();
   }
 };
